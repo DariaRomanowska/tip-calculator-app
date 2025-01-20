d46709b2a9113a572a6a13a4ff42498eb25a7ef9
DariaRomanowska <mmmao2212@gmail.com>
2025-01-20 14:11:11 +0100

    finish the project

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const person = document.getElementById('person');
 const tipBtns = document.querySelectorAll('.btn-tip')
 const tipText = document.querySelector('#tip-amount');
 const totalText = document.querySelector('#total');
+const resetBtn = document.querySelector('#reset');
 let billAmount = 0;
 let personAmount = 0;
 let tipAmount = 0;
@@ -12,26 +13,56 @@ let totalPerPerson = 0;
 const customTipBtn = document.getElementById('custom');
 
 function calculateTip(bill, tip, person) {
-    tipPerPerson = ((bill * tip) / 100) / person;
-    tipText.textContent = `${parseFloat(tipPerPerson.toFixed(2))}`;
-    return tipPerPerson
+    if(person > 0){
+        tipPerPerson = ((bill * tip) / 100) / person;
+        tipText.textContent = `${parseFloat(tipPerPerson.toFixed(2))}`;
+        return tipPerPerson
+    } else {
+        console.log('persons must be more than 0')
+    }
 }
 
 function calculateTotal(bill, tip, person) {
-    totalPerPerson = (bill / person) + tipPerPerson;
-    totalText.textContent = `${parseFloat(totalPerPerson.toFixed(2))}`;
+    if(person > 0){
+        totalPerPerson = (bill / person) + tipPerPerson;
+        totalText.textContent = `${parseFloat(totalPerPerson.toFixed(2))}`;
+    } else {
+        console.log('persons must be more than 0')
+    }
+
 }
 
+
+function getReset () {
+    resetBtn.classList.add('active-reset');
+    resetBtn.addEventListener('click', () => {
+        resetBtn.classList.remove('active-reset');
+        tipBtns.forEach((btn) => {
+            btn.classList.remove('active-btn')
+        })
+        tipText.textContent = '0.00'
+        totalText.textContent = '0.00'
+        billAmount = 0;
+        personAmount = 0;
+        tipAmount = 0;
+        document.querySelector('form').reset();
+    })
+}
 function getAmount() {
     tipBtns.forEach((btn) => {
         btn.addEventListener('click', () => {
+            tipBtns.forEach((btn) => {
+                btn.classList.remove('active-btn')
+            })
             btn.classList.add('active-btn')
             tipAmount = btn.getAttribute('data-value');
             if (billAmount === 0 || personAmount === 0) {
-                tipText.textContent = '0.00'
+                tipText.textContent = '0.00';
+                totalText.textContent = '0.00';
             } else {
                 calculateTip(billAmount, tipAmount, personAmount);
                 calculateTotal(billAmount, tipPerPerson, personAmount);
+                getReset()
                 // tipText.textContent = `${(((billAmount * tipAmount) / 100) / personAmount)}`
             }
         })
@@ -41,10 +72,12 @@ function getAmount() {
         tipAmount = customTipBtn.value
         if (billAmount === 0 || personAmount === 0) {
             tipText.textContent = '0.00'
+            totalText.textContent = '0.00'
         } else {
             // tipText.textContent = `${((billAmount * tipAmount)/100)/personAmount}`
             calculateTip(billAmount, tipAmount, personAmount);
             calculateTotal(billAmount, tipPerPerson, personAmount);
+            getReset()
         }
     })
 
@@ -53,11 +86,13 @@ function getAmount() {
         bill.setAttribute('data-value', `${bill.value}`)
         billAmount = bill.getAttribute('data-value')
         if (tipAmount === 0 || personAmount === 0) {
-            tipText.textContent = '0.00'
+            tipText.textContent = '0.00';
+            totalText.textContent = '0.00';
         } else {
             // tipText.textContent = `${((billAmount * tipAmount)/100)/personAmount}`
             calculateTip(billAmount, tipAmount, personAmount);
             calculateTotal(billAmount, tipPerPerson, personAmount);
+            getReset()
         }
     })
 
@@ -65,13 +100,17 @@ function getAmount() {
         person.setAttribute('data-value', `${person.value}`)
         personAmount = person.getAttribute('data-value')
         if (tipAmount === 0 || billAmount === 0) {
-            tipText.textContent = '0.00'
+            tipText.textContent = '0.00';
+            totalText.textContent = '0.00';
         } else {
             // tipText.textContent = `${((billAmount * tipAmount)/100)/personAmount}`
             calculateTip(billAmount, tipAmount, personAmount);
             calculateTotal(billAmount, tipPerPerson, personAmount);
+            getReset()
         }
     })
 }
 
 getAmount()
+
+
